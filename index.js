9368da0b9bfba89939c3e14bc268ed878f339ad2
Charlie Chung <charlieschung@gmail.com>
2019-07-16 17:31:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,12 +10,17 @@ function logShout(string){
 function logWhisper(string){
   console.log(string.toLowerCase());
 }
-function sayHiToGrandma(string){
+/*function sayHiToGrandma(string){
   if (string===string.toLowerCase) {
-    return "I can\'t hear you!";
+    return "I can't hear you!";
   } else if (string===string.toUpperCase) {
     return 'YES INDEED!';
   } else if (string === "I love you, Grandma.") {
     return "I love you, too.";
+  }
+}*/
+function sayHiToGrandma(string){
+  if (string===string.toLowerCase){
+    return "I can't hear you!";
   }
 }
